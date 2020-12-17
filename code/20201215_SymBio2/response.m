41d238d92d7dc6497280f4dad0fa28b7994acb16
RA <numpde@null.net>
2020-12-18 00:33:30 +0100

    PlayerA draft

---
+++ code/20201215_SymBio2/response.m
@@ -0,0 +1,47 @@
+% 2020-HS Intro Bio Computers
+% RA, 2020-12-17
+
+clear all;
+close all;
+
+m1 = sbioloadproject("PlayerA.sbproj").m1;
+
+index_of = containers.Map();
+for i = 1 : length(m1.Species)
+	index_of(m1.Species(i).Name) = i;
+end
+
+m1.Species(index_of('pC')).InitialAmount = 1;
+
+
+aa = logspace(-2, 2, 9);
+bb = logspace(-2, 2, 10);
+
+A = "Sal";
+B = "3OC6";
+
+responses = {};
+
+for a = aa
+	for b = bb
+		m1.Species(index_of(A)).InitialAmount = a;
+		m1.Species(index_of(B)).InitialAmount = b;
+		[t, x] = sbiosimulate(m1);
+		% for R = convertCharsToStrings({m1.Species.name})
+		for r = (1 : length(m1.Species))
+			responses{r}(a == aa, b == bb) = x(end, r);
+		end
+	end
+end
+
+for R = ["PhlF", "BetI", "DAPG"]
+	figure;
+	surf(aa, bb, responses{index_of(R)}');
+	xlabel(A);
+	ylabel(B);
+	title(R);
+	
+	ax = gca;
+	ax.XScale = 'log';
+	ax.YScale = 'log';
+end
