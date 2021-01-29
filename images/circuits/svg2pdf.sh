36da760cd2e61e21428acaf872226b75b5ac8b33
RA <numpde@null.net>
2021-01-29 16:18:16 +0100

    revise img

--- images/circuits/svg2pdf.sh
+++ images/circuits/svg2pdf.sh
@@ -4,6 +4,7 @@ for i in *.svg
 do
 j="${i}.pdf"
 echo "${i}" "-->" "${j}"
-inkscape -D -z --file="${i}" --export-pdf="${j}" --export-margin=1
+#inkscape -D -z --file="${i}" --export-pdf="${j}" --export-margin=1
+rsvg-convert -f pdf -o "${j}" "${i}"
 done
 
